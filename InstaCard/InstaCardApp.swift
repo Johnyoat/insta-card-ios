296a758e56bffc6dde21a08e3c15cf664f0e99f2
Johnson Osei Yeboah <johnyoat@gmail.com>
2021-04-07 11:56:49 +0000

    Initial Commit

---
+++ InstaCard/InstaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCardApp.swift
+//  InstaCard
+//
+//  Created by Johnson Osei Yeboah on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
