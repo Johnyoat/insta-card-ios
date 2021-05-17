6fcfbf4de917a0d194ec3875ae574970dde3937a
Johnson Osei Yeboah <johnyoat@gmail.com>
2021-05-17 21:16:00 +0000

    feat: 🔧 service card

--- InstaCard/ContentView.swift
+++ InstaCard/ContentView.swift
@@ -9,6 +9,29 @@ import SwiftUI
 import Lottie
 
 struct ContentView: View {
+    fileprivate func ServiceCard(icon : String, title :String, index: Int) -> some View {
+        return VStack(spacing: 16){
+            Image(icon)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 48, height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            
+            Text(title)
+                .foregroundColor(.white)
+                .font(.caption)
+            
+        }.padding(16)
+        .frame(
+            maxWidth: .infinity)
+        .background(Color("litGray"))
+        .cornerRadius(8)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/ )
+        .padding(.bottom,16)
+        .padding(.leading,8)
+        .padding(.trailing,8)
+    }
+    
     var body: some View {
         ZStack(alignment: .top){
             Color("blackGray").ignoresSafeArea(.all)
@@ -32,7 +55,25 @@ struct ContentView: View {
                     
                 }.padding(.leading)
                 .padding(.trailing)
+                
                 CardView()
+                
+                HStack{
+                    ServiceCard(icon: "transfer_money", title: "Transfer Money", index: 1)
+                    
+                    ServiceCard(icon: "bill", title: "Pay Bills", index: 2)
+                    
+                }.padding(.leading,8)
+                .padding(.trailing,8)
+                
+                HStack{
+                    ServiceCard(icon: "airtime", title: "Buy Airtime", index: 3)
+                    
+                    ServiceCard(icon: "transaction", title: "Transaction", index: 4)
+                    
+                }.padding(.leading,8)
+                .padding(.trailing,8)
+                
             
         
             }
